1a4d7a7c53665862ab1037ca9260635c86d1d554
AshishCoreProject <ashishbarthwal1799@gmail.com>
2023-12-20 17:42:29 +0530

    useSWR hook for fetching data instead of custom code we use to write

--- pages/last-sale.js
+++ pages/last-sale.js
@@ -1,39 +1,41 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 function LastSalePage() {
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(async function () {
-    setIsLoading(true);
-    const response = await fetch(
-      "https://nextjsproject-210c6-default-rtdb.firebaseio.com/sales.json"
-    );
-    const data = await response.json();
-    const transformedSales = [];
+  const { data, error } = useSWR(
+    "https://nextjsproject-210c6-default-rtdb.firebaseio.com/sales.json"
+  );
+  console.log(data);
 
-    for (const key in data) {
-      transformedSales.push({
-        id: key,
-        username: data[key].username,
-        volume: data[key].volume,
-      });
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
     }
-    setSales(transformedSales);
-    setIsLoading(false);
-  }, []);
+  }, [data]);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
+  if (error) {
+    return <p>Failed to Load</p>;
   }
-  if (!sales) {
-    return <p> No data is there</p>;
+
+  if (!data || !sales) {
+    return <p>Loading...</p>;
   }
 
   return (
     <>
       <ul>
-        {sales?.map((sale) => (
+        {sales.map((sale) => (
           <li key={sale.id}>{sale.username}</li>
         ))}
       </ul>
